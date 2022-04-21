793caf03a9ca9b2469c84c6738cd8a1add5ae0b1
Sheng Wang <sheng@a64.work>
2022-04-21 20:17:21 +0900

    fetch not only the current month

--- nogi-ical-worker/src/handler.ts
+++ nogi-ical-worker/src/handler.ts
@@ -1,8 +1,9 @@
 import { nogiSource } from './sources/nogi'
 import { motoSource } from './sources/moto'
-import { Source } from './source'
+import { Source, SourceTimeSpan } from './source'
 import ical from 'ical-generator'
 import { ICalEventData } from 'ical-generator'
+import { dateAddHours } from './utils'
 
 enum Bitmask{
   NOGI = 1,
@@ -38,6 +39,44 @@ function parseSourcesFromUrl(url: string): Source[] {
   return sources
 }
 
+async function fetchFuture(source: Source): Promise<ICalEventData[]> {
+  let now = new Date()
+  const events = []
+  switch (source.fetchSpan) {
+    case SourceTimeSpan.Day:
+      // get today and the next 6 days
+      for (let i = 0; i < 7; i++) {
+        const evs = await source.fetch(now)
+        events.push(...evs)
+        dateAddHours(now, 24) // add 7 days
+      }
+      break
+    case SourceTimeSpan.Week:
+      // get this week and the next 3 weeks
+      for (let i = 0; i < 4; i++) {
+        const evs = await source.fetch(now)
+        events.push(...evs)
+        dateAddHours(now, 24*7) // add 7 days
+      }
+      break
+    case SourceTimeSpan.Month:
+      // get this month
+      const evs = await source.fetch(now)
+      events.push(...evs)
+      // get the next month if current day > 15
+      if (now.getDate() > 15) {
+        dateAddHours(now, 24*20) // 20 days after the 15th day will land in the next month
+        const evs = await source.fetch(now)
+        events.push(...evs)
+      }
+      break
+    default:
+      console.error(`Impossible enum value for SourceTimeSpan: ${source.fetchSpan}`)
+      break
+  }
+  return events
+}
+
 export async function handleRequest(event: FetchEvent): Promise<Response> {
   const request = event.request
   const cache = caches.default
@@ -55,13 +94,13 @@ export async function handleRequest(event: FetchEvent): Promise<Response> {
     let name = ""
     for (const source of sources) {
       console.log(`Fetch from source:${source.name}`)
-      const evs = await source.fetch(now)
+      const evs = await fetchFuture(source)
       // we can further optimize by preallocating events
       events.push(...evs)
       name += source.name
     }
 
-    const calendar = ical({ name: 'Nogi' })
+    const calendar = ical({ name: name })
     for (const ev of events) {
       calendar.createEvent(ev)
     }
